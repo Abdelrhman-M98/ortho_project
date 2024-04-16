2f6ab18d4579c323a33cc7befa53de99aa0f79ac
Abdelrahman Mahmoud <abdelrhmanmahmo0ud@gmail>
2024-04-17 00:07:22 +0200

    add verification screen without any controllers

--- lib/screens/Virification_Page.dart
+++ lib/screens/Virification_Page.dart
@@ -1,14 +1,62 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, file_names
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:ortho/components/AppColors.dart';
 import 'package:ortho/components/Btn_widget.dart';
 import 'package:ortho/components/CustomAppBar.dart';
 import 'package:ortho/screens/Login_page.dart';
-import 'package:pin_code_fields/pin_code_fields.dart'; // Import PinCodeTextField
+import 'package:pinput/pinput.dart'; // Import PinCodeTextField
 
-class VerificationPage extends StatelessWidget {
-  const VerificationPage({Key? key});
+class VerificationPage extends StatefulWidget {
+  const VerificationPage();
+
+  @override
+  _VerificationPageState createState() => _VerificationPageState();
+}
+
+class _VerificationPageState extends State<VerificationPage> {
+  late Timer _timer;
+  int _start = 3;
+  bool isPinCorrect = false;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (_start == 0) {
+          setState(() {
+            timer.cancel();
+// Set _resnd to 0 when countdown reaches zero
+          });
+        } else {
+          setState(() {
+            _start--;
+          });
+        }
+      },
+    );
+  }
+
+  void restartTimer() {
+    setState(() {
+      _start = 60;
+      startTimer();
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,73 +103,131 @@ class VerificationPage extends StatelessWidget {
             const SizedBox(
               height: 24,
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: PinCodeTextField(
-                appContext: context,
-                length: 5,
-                textStyle: const TextStyle(
-                  fontFamily: 'Nunito',
-                  color: AppColors.Pin_Text,
-                  fontSize: 26,
-                  letterSpacing: 0,
-                  height: 1.2,
-                ),
+            Center(
+              child: Pinput(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                enableActiveFill: false,
-                autoFocus: true,
-                enablePinAutofill: true,
-                errorTextSpace: 16,
-                showCursor: false,
-                cursorColor: AppColors.cursorColor,
-                obscureText: false,
-                keyboardType: TextInputType.number,
-                pinTheme: PinTheme(
-                  fieldHeight: 67,
-                  fieldWidth: 50,
-                  borderWidth: 1.33,
-                  borderRadius: BorderRadius.circular(72),
-                  shape: PinCodeFieldShape.box,
-                  activeColor: AppColors.Pin_full_color,
-                  inactiveColor: AppColors.Pin_Empty_color,
-                  selectedColor: AppColors.Pin_full_color,
-                  selectedFillColor: AppColors.Pin_full_color,
-                ),
-                onChanged: (value) {
-                  if (value == '12345') {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => LoginPage(),
-                      ),
-                    );
+                length: 5,
+                onCompleted: (pin) {
+                  if (pin == '12345') {
+                    isPinCorrect = true;
+                    return;
                   }
                 },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (_) {
-                  return null;
+                validator: (pin) {
+                  if (pin == '12345') {
+                    return null;
+                  } else {
+                    return "error";
+                  }
                 },
+                errorText:
+                    "The verification code that you entered is incorrect , Try again ",
+                errorTextStyle: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "Nunito",
+                    color: AppColors.Pin_error_color),
+                defaultPinTheme: PinTheme(
+                  width: 50,
+                  height: 67,
+                  textStyle: const TextStyle(
+                      fontSize: 26,
+                      color: AppColors.dark_text,
+                      fontWeight: FontWeight.w500),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.Pin_Empty_color),
+                    borderRadius: BorderRadius.circular(72),
+                  ),
+                ),
+                focusedPinTheme: PinTheme(
+                  width: 50,
+                  height: 67,
+                  textStyle: const TextStyle(
+                      fontSize: 26,
+                      color: AppColors.dark_text,
+                      fontWeight: FontWeight.w500),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.Pin_full_color),
+                    borderRadius: BorderRadius.circular(72),
+                  ),
+                ),
+                errorPinTheme: PinTheme(
+                  width: 50,
+                  height: 67,
+                  textStyle: const TextStyle(
+                      fontSize: 26,
+                      color: AppColors.dark_text,
+                      fontWeight: FontWeight.w500),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.Pin_error_color),
+                    borderRadius: BorderRadius.circular(72),
+                  ),
+                ),
               ),
             ),
             const SizedBox(
-              height: 14,
+              height: 25,
             ),
-            const Text(
-              "resend verification code  (0:55) ",
-              style: TextStyle(
-                fontFamily: "Nunito",
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Resend verification code",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "Nunito",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                _start != 0
+                    ? Text(
+                        "(0: $_start)",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontFamily: "Nunito",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    : InkWell(
+                        onTap: () {
+                          restartTimer(); // Call restartTimer function
+                        },
+                        child: const Text(
+                          "Resend",
+                          style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 16,
+                            color: AppColors.btn_background,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+              ],
             ),
             SizedBox(
-              height: MediaQuery.of(context).viewInsets.bottom > 0 ? 225 : 352,
+              height: MediaQuery.of(context).viewInsets.bottom > 0 ? 190 : 350,
             ),
-            SizedBox(
-              width: 360,
-              height: 60,
-              child: BtnWidget(
-                btnText: "Verify OTP",
-                onTap: () {},
+            Center(
+              child: SizedBox(
+                width: 360,
+                height: 60,
+                child: BtnWidget(
+                  btnText: "Verify OTP",
+                  onTap: () {
+                    if (isPinCorrect == true) {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    }
+                  },
+                ),
               ),
             ),
           ],
