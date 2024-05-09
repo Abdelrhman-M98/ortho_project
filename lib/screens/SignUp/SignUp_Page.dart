fc5a3f8226e5bae709b4aa7932a891509cc5d14c
Mahmoud Adawy <mahmoudadawy842@gmail.com>
2024-05-09 09:55:01 +0300

    handle signup

--- lib/screens/SignUp/SignUp_Page.dart
+++ lib/screens/SignUp/SignUp_Page.dart
@@ -20,7 +20,6 @@ class SignUpPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final emailController = useTextEditingController();
     final nameField = useTextEditingController();
-    final formKey = useMemoized(() => GlobalKey<FormState>());
     final authProviderNotifier = ref.watch(authProvider.notifier);
     final authProviderState = ref.watch(authProvider);
     ref.listen(authProvider, (previous, next) {
@@ -90,177 +89,186 @@ class SignUpPage extends HookConsumerWidget {
           ),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 20.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Container(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Hi there, welcome to Ortho AI ",
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20.sp,
-                  color: const Color(0xff15331b),
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.h,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Hi there, welcome to Ortho AI ",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontFamily: 'Nunito',
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20.sp,
+                    color: const Color(0xff15331b),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 10.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              "creating your account will take only few minutes to ensure to you a better experience ",
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                fontFamily: 'Nunito',
-                fontWeight: FontWeight.w500,
-                fontSize: 16.sp,
-                color: AppColors.SecondaryColor,
+            SizedBox(
+              height: 10.h,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                "creating your account will take only few minutes to ensure to you a better experience ",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.sp,
+                  color: AppColors.SecondaryColor,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 40.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: InputFeild(
-              setErrorSate: authProviderState.userNameErrors.length > 0,
-              showSuffixIcon: true,
-              titel: 'Name',
-              controller: nameField,
-              validator: (value) {
-                return null;
-              },
+            SizedBox(
+              height: 40.h,
             ),
-          ),
-          authProviderState.hasErrors
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment
-                        .end, // Align items horizontally to the end (right side)
-                    children: [
-                      SizedBox(
-                        height: 30.0 * authProviderState.userNameErrors.length,
-                        child: ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: authProviderState.userNameErrors.length,
-                          itemBuilder: (context, index) {
-                            return Text(
-                              authProviderState.userNameErrors[index],
-                              style: TextStyle(
-                                fontSize: 12.sp,
-                                fontFamily: "Nunito",
-                                fontWeight: FontWeight.w500,
-                                color: AppColors.Fail_Text,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : Container(),
-          SizedBox(
-            height: 15.h,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-            ),
-            child: InputFeild(
-              setErrorSate: authProviderState.userMailErrors.length > 0,
-              showSuffixIcon: true,
-              titel: 'Eamil Adderss',
-              controller: emailController,
-              validator: (value) {
-                return;
-              },
-            ),
-          ),
-          authProviderState.hasErrors
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment
-                        .end, // Align items horizontally to the end (right side)
-                    children: [
-                      SizedBox(
-                        height: 30.0 * authProviderState.userMailErrors.length,
-                        child: ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: authProviderState.userMailErrors.length,
-                          itemBuilder: (context, index) {
-                            return Text(
-                              authProviderState.userMailErrors[index],
-                              style: TextStyle(
-                                fontSize: 12.sp,
-                                fontFamily: "Nunito",
-                                fontWeight: FontWeight.w500,
-                                color: AppColors.Fail_Text,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : Container(),
-          const Spacer(
-            flex: 50,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: const Padding(
-              padding: EdgeInsets.only(
-                left: 16.0,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: InputFeild(
+                setErrorSate: authProviderState.userNameErrors.length > 0,
+                showSuffixIcon: true,
+                titel: 'Name',
+                controller: nameField,
+                validator: (value) {
+                  return null;
+                },
               ),
-              child: TermsOfUse(),
             ),
-          ),
-          const Spacer(),
-          Spinner_BTN(
-            btnText: authProviderState.isLoading ? 'Loading...' : 'Sign Up',
-            onTap: () {
-              if (authProviderState.isLoading) {
-                return;
-              }
+            authProviderState.hasErrors
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment
+                          .end, // Align items horizontally to the end (right side)
+                      children: [
+                        SizedBox(
+                          height:
+                              30.0 * authProviderState.userNameErrors.length,
+                          child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: authProviderState.userNameErrors.length,
+                            itemBuilder: (context, index) {
+                              return Text(
+                                authProviderState.userNameErrors[index],
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  fontFamily: "Nunito",
+                                  fontWeight: FontWeight.w500,
+                                  color: AppColors.Fail_Text,
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
+            SizedBox(
+              height: 15.h,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: InputFeild(
+                setErrorSate: authProviderState.userMailErrors.length > 0,
+                showSuffixIcon: true,
+                titel: 'Eamil Adderss',
+                controller: emailController,
+                validator: (value) {
+                  return;
+                },
+              ),
+            ),
+            authProviderState.hasErrors
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment
+                          .end, // Align items horizontally to the end (right side)
+                      children: [
+                        SizedBox(
+                          height:
+                              30.0 * authProviderState.userMailErrors.length,
+                          child: ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: authProviderState.userMailErrors.length,
+                            itemBuilder: (context, index) {
+                              return Text(
+                                authProviderState.userMailErrors[index],
+                                style: TextStyle(
+                                  fontSize: 12.sp,
+                                  fontFamily: "Nunito",
+                                  fontWeight: FontWeight.w500,
+                                  color: AppColors.Fail_Text,
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Container(),
+            SizedBox(
+              height:
+                  MediaQuery.of(context).viewInsets.bottom > 0 ? 50.h : 280.h,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: 16.0,
+                ),
+                child: TermsOfUse(),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Spinner_BTN(
+              btnText: authProviderState.isLoading ? 'Loading...' : 'Sign Up',
+              onTap: () {
+                if (authProviderState.isLoading) {
+                  return;
+                }
 
-              if (!authProviderState.hasErrors &&
-                  authProviderState.token != null) {
-                return;
-              }
+                if (!authProviderState.hasErrors &&
+                    authProviderState.token != null) {
+                  return;
+                }
 
-              final email = emailController.text;
-              final name = nameField.text;
-              final signUpReqData = SignUpRequest(
-                email: email,
-                name: name,
-              );
-              authProviderNotifier.requestRegister(signUpReqData);
-            },
-            isLoading: authProviderState.isLoading,
-          ),
-          SizedBox(
-            height: 20.h,
-          ),
-        ],
+                final email = emailController.text;
+                final name = nameField.text;
+                final signUpReqData = SignUpRequest(
+                  email: email,
+                  name: name,
+                );
+                authProviderNotifier.requestRegister(signUpReqData);
+              },
+              isLoading: authProviderState.isLoading,
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+          ],
+        ),
       ),
     );
   }
